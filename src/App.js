bb9e587fbe52c9e09fae1bb4d32dad8627408745
robdcampbell <rdeancampbell@gmail.com>
2021-02-01 15:04:04 -0500

    structure and style header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Homepage Test</h1>
+      <Header />
+      {/* Header */}
+      {/* item 1  */}
+      {/* item 2 */}
+      {/* item 3  */}
+      {/* item 4 */}
     </div>
   );
 }
